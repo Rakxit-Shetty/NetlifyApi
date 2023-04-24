52ab8d7fe47f07fa4f913f67238a2cf1a30f1a50
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-04-24 21:33:40 +0530

    cors update

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,8 @@ const express=require('express');
 const serverless=require('serverless-http');
 const app=express();
 const router=express.Router();
-
+var cors = require('cors');
+ app.use(cors());
 router.get('/',(req,res)=>{
     res.json({"CODE":123});
 })
